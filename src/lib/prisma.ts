fbe92f1bf14515a1fddae92b2e41ceca85e0c31a
Abhishek Shahi <shahiabhi9900@gmail.com>
2025-12-19 23:46:32 +0530

    User Synced wITH Database

---
+++ src/lib/prisma.ts
@@ -0,0 +1,11 @@
+//create a single instance of PrismaClient to be used across the app
+
+
+import { PrismaClient } from '@prisma/client'
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
+
+export const prisma =
+  globalForPrisma.prisma || new PrismaClient()
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
